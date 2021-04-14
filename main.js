00e579c1a9fd720acccc7543dd7e4fc117186ccf
captressketh001 <shininglite2012@gmail.com>
2021-04-14 09:52:55 +0100

    Final commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-    function setTheme(themeName) {
-            localStorage.setItem('theme', themeName);
-            document.documentElement.className = themeName;
-        }
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
 
-        // function to toggle between light and dark theme
+// function to toggle between light and dark theme
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
             setTheme('theme-light');
